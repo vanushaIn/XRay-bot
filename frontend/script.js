95f71e5ed8390781be96ba4bdb3c2ae97cf6cc70
vanushaIn <bibillegy@gmail.com>
2026-03-19 22:57:57 +0500

    Initial commit

--- frontend/script.js
+++ frontend/script.js
@@ -6,7 +6,7 @@ tg.expand();
 const initData = tg.initData;
 
 // URL вашего бэкенда (замените на реальный)
-const API_BASE = 'https://ваш-домен.com'; // например, https://ваш-домен.ru
+const API_BASE = ''; // например, https://ваш-домен.ru
 
 // Элементы DOM
 const statusIcon = document.getElementById('status-icon');
